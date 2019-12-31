e56d3b4971b32f9600fdecdcf33b4cca2da45694
RussCoder <RussCoder@yandex.ru>
2019-12-31 17:59:37 +0300

    Documentation corrections. Unified breakpoints in the examples. Added css rules for clarity.

--- examples/ssr-rendering/src/Media.tsx
+++ examples/ssr-rendering/src/Media.tsx
@@ -2,10 +2,10 @@ import { createMedia } from "@artsy/fresnel"
 
 const ExampleAppMedia = createMedia({
   breakpoints: {
-    xs: 0,
-    sm: 768,
-    md: 1000,
-    lg: 1200,
+    sm: 0,
+    md: 768,
+    lg: 1024,
+    xl: 1192,
   },
 })
 
