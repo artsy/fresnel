14c497c42e339e91c3f14b452ed1ff5ad953fb2d
Justin Bennett <zephraph@gmail.com>
2020-09-02 08:23:52 -0400

    Fix docs and add orientation interact to ssr exmpl

--- examples/ssr-rendering/src/App.tsx
+++ examples/ssr-rendering/src/App.tsx
@@ -4,6 +4,16 @@ import { Media, MediaContextProvider } from "./Media"
 export const App = () => {
   return (
     <MediaContextProvider>
+      <Media interaction="hover">
+        media has hover interaction capabilities
+      </Media>
+      <Media interaction="notHover">
+        media lacks hover interaction capabilities
+      </Media>
+
+      <Media interaction="landscape">landscape</Media>
+      <Media interaction="portrait">portrait</Media>
+
       <Media at="sm">Hello mobile!</Media>
       <Media greaterThan="sm">Hello desktop!</Media>
     </MediaContextProvider>
